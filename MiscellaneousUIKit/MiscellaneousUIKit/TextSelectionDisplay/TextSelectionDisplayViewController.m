f4d50749446525959aaef8f9e1ec31125c975f70
Jinwoo Kim <kidjinwoo@me.com>
2023-06-20 00:06:58 +0900

    no message

--- MiscellaneousUIKit/MiscellaneousUIKit/TextSelectionDisplay/TextSelectionDisplayViewController.m
+++ MiscellaneousUIKit/MiscellaneousUIKit/TextSelectionDisplay/TextSelectionDisplayViewController.m
@@ -6,6 +6,9 @@
 //
 
 #import "TextSelectionDisplayViewController.h"
+#import <objc/message.h>
+#import <objc/runtime.h>
+#import "NSObject+Foundation_IvarDescription.h"
 
 @interface TextSelectionDisplayCursorView : UIView <UITextCursorView>
 @end
@@ -14,28 +17,86 @@
     BOOL _isBlinking;
 }
 
++ (void)load {
+    class_addProtocol(TextSelectionDisplayCursorView.class, NSProtocolFromString(@"_UITextSelectionWidgetAnimating"));
+}
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        self.layer.backgroundColor = UIColor.purpleColor.CGColor;
+        [self startAnimation];
+    }
+    
+    return self;
+}
+
 - (BOOL)isBlinking {
     return _isBlinking;
 }
 
 - (void)setBlinking:(BOOL)blinking {
     _isBlinking = blinking;
+    
+    if (blinking) {
+        [self startAnimation];
+    }
 }
 
 - (void)resetBlinkAnimation {
+    [self startAnimation];
+}
+
+- (void)startAnimation {
+    [self stopAnimation];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animation.fromValue = @0.f;
+    animation.toValue = @1.f;
+    animation.duration = 1.f;
+    animation.repeatCount = HUGE_VALF;
+    
+    [self.layer addAnimation:animation forKey:@"blink"];
+}
+
+- (void)stopAnimation {
+    [self.layer removeAnimationForKey:@"blink"];
+}
+
+#pragma mark - _UITextSelectionWidgetAnimating
+
+- (BOOL)hiddenForLoupeAnimation {
+    return NO;
+}
+
+- (CGSize)originShadow {
+    return CGSizeZero;
+}
+
+- (CGRect)originShape {
+    return CGRectZero;;
+}
+
+- (void)setHiddenForLoupeAnimation:(BOOL)arg1 {
     
 }
 
-- (void)didMoveToSuperview {
-    [super didMoveToSuperview];
+@end
+
+@interface TextSelectionDisplayHighlightView : UIView <UITextSelectionHighlightView>
+@end
+
+@implementation TextSelectionDisplayHighlightView
+
+- (instancetype)init {
+    if (self = [super init]) {
+        
+    }
+    
+    return self;
 }
 
-@end
-
-@interface TextSelectionDisPlayHighlightView : UIView <UITextSelectionHighlightView>
-@end
-
-@implementation TextSelectionDisPlayHighlightView
 
 - (NSArray<UITextSelectionRect *> *)selectionRects {
     return @[];
@@ -43,10 +104,6 @@
 
 - (void)setSelectionRects:(NSArray<UITextSelectionRect *> *)selectionRects {
     
-}
-
-- (void)didMoveToSuperview {
-    [super didMoveToSuperview];
 }
 
 @end
@@ -69,17 +126,26 @@
     UITextView *textView = [[UITextView alloc] initWithFrame:self.view.bounds];
     textView.translatesAutoresizingMaskIntoConstraints = NO;
     
+    
+    [textView.interactions enumerateObjectsUsingBlock:^(id<UIInteraction>  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:UITextSelectionDisplayInteraction.class]) {
+            [textView removeInteraction:obj];
+            *stop = YES;
+//            UITextSelectionDisplayInteraction *interaction = (UITextSelectionDisplayInteraction *)obj;
+//            ((void (*)(id, SEL, BOOL))objc_msgSend)(interaction.cursorView, NSSelectorFromString(@"setGlowEffectEnabled:"), YES);
+        }
+    }];
+    
     UITextSelectionDisplayInteraction *interaction = [[UITextSelectionDisplayInteraction alloc] initWithTextInput:textView delegate:self];
     TextSelectionDisplayCursorView *cursorView = [TextSelectionDisplayCursorView new];
     interaction.cursorView = cursorView;
     [cursorView release];
     
-    TextSelectionDisPlayHighlightView *highlightView = [TextSelectionDisPlayHighlightView new];
+    TextSelectionDisplayHighlightView *highlightView = [TextSelectionDisplayHighlightView new];
     interaction.highlightView = highlightView;
     [highlightView release];
     
     [textView addInteraction:interaction];
-    
     
     interaction.activated = YES;
     [interaction release];
