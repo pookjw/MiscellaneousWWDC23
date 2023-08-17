7a4924d2f45da55a4ee53a5d8bdc71cd5f607a13
Jinwoo Kim <kidjinwoo@me.com>
2023-08-17 23:31:00 +0900

    no message

---
+++ MiscellaneousSwiftUI-watchOS/MiscellaneousSwiftUI-watchOS Watch App/MiscellaneousSwiftUI_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiscellaneousSwiftUI_watchOSApp.swift
+//  MiscellaneousSwiftUI-watchOS Watch App
+//
+//  Created by Jinwoo Kim on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MiscellaneousSwiftUI_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
